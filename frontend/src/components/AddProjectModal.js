bf8ff0d100dc069da9ebe98124cf3dcdf6dd1338
Sahil Gaikwad <121560774+DragoonKing@users.noreply.github.com>
2023-09-08 14:01:14 +0530

    Update AddProjectModal.js
    added deployed backend

--- frontend/src/components/AddProjectModal.js
+++ frontend/src/components/AddProjectModal.js
@@ -12,7 +12,7 @@ const AddProjectModal = ({ isModalOpen, closeModal, edit = false, id = null }) =
 
     useEffect(() => {
         if (edit && isModalOpen) {
-            axios.get(`http://localhost:9000/project/${id}`)
+            axios.get(`https://to-do-backend-rli3.onrender.com/project/${id}`)
                 .then((res) => {
                     setTitle(res.data[0].title)
                     setDesc(res.data[0].description)
@@ -27,7 +27,7 @@ const AddProjectModal = ({ isModalOpen, closeModal, edit = false, id = null }) =
     const handleSubmit = (e) => {
         e.preventDefault()
         if (!edit) {
-            axios.post('http://localhost:9000/project/', { title, description: desc })
+            axios.post('https://to-do-backend-rli3.onrender.com/project/', { title, description: desc })
                 .then((res) => {
                     closeModal()
                     const customEvent = new CustomEvent('projectUpdate', { detail: { ...res.data } });
